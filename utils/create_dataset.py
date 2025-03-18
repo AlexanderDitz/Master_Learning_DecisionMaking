4da21886c291a4515462226306b532a5d7ec2b32
whyhardt <daniel.weinhardt@uni-osnabrueck.de>
2025-03-18 16:17:34 +0100

    minor fix

--- utils/create_dataset.py
+++ utils/create_dataset.py
@@ -50,13 +50,13 @@ for iteration in range(n_iterations_per_n_sessions):
         
         for i in range(len(dataset)):    
             # get update dynamics
-            experiment = dataset.xs[i]
+            experiment = dataset.xs[i].cpu().numpy()
             qs, choice_probs, _ = get_update_dynamics(experiment, agent)
             
             # append behavioral data
-            session += list(experiment.session)
-            choice += list(experiment.choices)
-            reward += list(experiment.rewards[:, 0])
+            session += list(experiment[:, -1])
+            choice += list(np.argmax(experiment[:, :agent._n_actions], axis=-1))
+            reward += list(np.max(experiment[:, agent._n_actions:agent._n_actions*2], axis=-1))
             
             # append update dynamics
             choice_prob_0 += list(choice_probs[:, 0])
