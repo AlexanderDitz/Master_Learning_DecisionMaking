fixed beta scaling -> now outside of call_module (better because otherwise scaled values are stored in memory state); did also bunch of other stuff

resources/rnn.py
@@ -578,7 +578,7 @@ class RLRNN(BaseRNN):
                 inputs=(reward, learning_rate_reward),
                 participant_embedding=participant_embedding,
                 participant_index=participant_id,
-                scaling=True,
+                # scaling=True,
                 )
             
             next_value_reward_not_chosen = self.call_module(
@@ -599,7 +599,7 @@ class RLRNN(BaseRNN):
                 participant_embedding=participant_embedding,
                 participant_index=participant_id,
                 activation_rnn=torch.nn.functional.sigmoid,
-                scaling=True,
+                # scaling=True,
                 )
             
             next_value_choice_not_chosen = self.call_module(
@@ -610,7 +610,7 @@ class RLRNN(BaseRNN):
                 participant_embedding=participant_embedding,
                 participant_index=participant_id,
                 activation_rnn=torch.nn.functional.sigmoid,
-                scaling=True,
+                # scaling=True,
                 )
             
             # updating the memory state
@@ -618,8 +618,14 @@ class RLRNN(BaseRNN):
             self.state['x_value_reward'] = next_value_reward_chosen + next_value_reward_not_chosen
             self.state['x_value_choice'] = next_value_choice_chosen + next_value_choice_not_chosen
             
+            # get scaling factors
+            scaling_factors = {}
+            for key in self.state:
+                if key in self.betas:
+                    scaling_factors[key] = self.betas[key] if isinstance(self.betas[key], nn.Parameter) else self.betas[key](participant_embedding)
+            
             # Now keep track of the logit in the output array
-            logits[timestep] = self.state['x_value_reward'] + self.state['x_value_choice']
+            logits[timestep] = self.state['x_value_reward'] * scaling_factors['x_value_reward'] + self.state['x_value_choice'] * scaling_factors['x_value_choice']
             
             # record the inputs for training SINDy later on
             self.record_signal('c_action', action)