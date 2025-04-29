3ae7d1ded79a4edcaa6c67d484bf4a51a3440f86
whyhardt <daniel.weinhardt@uni-osnabrueck.de>
2025-04-29 08:56:10 +0200

    couple of small updates

--- playground.py
+++ playground.py
@@ -1,26 +1,11 @@
-import itertools
+import pandas as pd
 
-def group_sequences_by_target(target_sequence, *sequences):
-    # Use itertools.groupby to find the indices of groups in the target sequence
-    groups = [(key, list(group)) for key, group in itertools.groupby(enumerate(target_sequence), key=lambda x: x[1])]
-    
-    # Extract the indices for each group
-    group_indices = [list(map(lambda x: x[0], group)) for _, group in groups]
-    
-    # Group each input sequence based on the indices
-    grouped_sequences = []
-    for seq in sequences:
-        grouped_sequences.append([[seq[i] for i in indices] for indices in group_indices])
-    
-    return grouped_sequences
+path = "Participant_export_Basic_Programming_in_Python.csv"
 
-# Example usage
-binary_target = [1, 1, 0, 0, 1, 1, 1, 0]
-sequence1 = [10, 20, 30, 40, 50, 60, 70, 80]
-sequence2 = [5, 15, 25, 35, 45, 55, 65, 75]
+mails = pd.read_csv(path, sep=";")["E-mail"]
 
-grouped_sequences = group_sequences_by_target(binary_target, sequence1, sequence2)
+str_mails = ""
+for mail in mails:
+    str_mails += mail + " "
 
-# Output the grouped sequences
-for idx, seq in enumerate(grouped_sequences, start=1):
-    print(f"Grouped Sequence {idx}: {seq}")
+print(str_mails)
--- tests/test_pipeline_sindy.py
+++ tests/test_pipeline_sindy.py
@@ -7,21 +7,21 @@ import pipeline_sindy
 
 agent_spice, features, loss = pipeline_sindy.main(
     
-    # data='data/parameter_recovery/data_128p_0.csv',
-    # model='params/parameter_recovery/params_128p_0.pkl',
+    data='data/parameter_recovery/data_32p_0.csv',
+    model='params/parameter_recovery/params_32p_0.pkl',
     
-    model = 'params/eckstein2022/params_eckstein2022.pkl',
-    data = 'data/eckstein2022/eckstein2022.csv',
+    # model = 'params/eckstein2022/params_eckstein2022_test.pkl',
+    # data = 'data/eckstein2022/eckstein2022.csv',
     
     # general recovery parameters
-    participant_id=1,
+    participant_id=2,
     
     # sindy parameters
     polynomial_degree=1,
-    optimizer_alpha=.1,
+    optimizer_alpha=0.05,
     optimizer_threshold=0.05,
     n_trials_off_policy=1000,
-    n_sessions_off_policy=2,
+    n_sessions_off_policy=0,
     verbose=True,
     
     # generated training dataset parameters
