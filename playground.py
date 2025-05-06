7c652efec0bb1d3fc16a6b94c5c45ee3a7ae2902
whyhardt <daniel.weinhardt@uni-osnabrueck.de>
2025-05-06 16:03:45 +0200

    adjusted rnn-optuna optimization to not use validation data + use SINDy BIC as metric

--- playground.py
+++ playground.py
@@ -1,4 +1,20 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('all_scores.csv', index_col=0)
-print(df['RNN'].mean())
+
+n_trials = df['Trials'].values
+spice_nll = df['SPICE'].values
+
+print((spice_nll/n_trials).mean())
+
+n_samples = 100
+nlls = []
+for _ in range(10):
+    # get n_samples random samples
+    index_samples = np.random.randint(0, len(df), n_samples)
+    nlls.append((spice_nll[index_samples]/n_trials[index_samples]).mean())
+
+print(nlls)
+print(np.mean(nlls))
+print(np.std(nlls))
