d86f54ca4dc40334028ac6073029e5f12c0eb946
whyhardt <daniel.weinhardt@uni-osnabrueck.de>
2025-05-15 00:05:21 +0200

    added gradient penalty; have to check now

--- tests/test_pipeline_rnn.py
+++ tests/test_pipeline_rnn.py
@@ -7,16 +7,16 @@ import pipeline_rnn
 
 _, loss = pipeline_rnn.main(
     checkpoint=False,
-    epochs=4096,#65536,
+    epochs=65536,
     
-    # data='data/parameter_recovery/data_256p_0.csv',
-    # model='params/parameter_recovery_2/rnn_256p_0.pkl',
+    data='data/parameter_recovery/data_256p_0.csv',
+    model='params/parameter_recovery_2/rnn_256p_0_l1_0_001_gp_10.pkl',
     
     # data = 'data/optuna/data_128p_0.csv',
     # model = 'params/params_128p_0.pkl',
     
-    model='params/eckstein2022/rnn_eckstein2022_l1_0_0001_l2_0_plateau.pkl',
-    data='data/eckstein2022/eckstein2022.csv',
+    # model='params/eckstein2022/rnn_eckstein2022_l1_0_001_gp_10.pkl',
+    # data='data/eckstein2022/eckstein2022.csv',
     
     n_actions=2,
     
@@ -28,9 +28,9 @@ _, loss = pipeline_rnn.main(
     # hand-picked params
     embedding_size=32,
     n_steps=32,
-    learning_rate=1e-3,
+    learning_rate=1e-4,
     l1_weight_decay=0.001,
-    l2_weight_decay=0.001,
+    l2_weight_decay=10.0,#.001,
     dropout=0.25,
     
     # toy params for quick run
@@ -40,7 +40,7 @@ _, loss = pipeline_rnn.main(
     
     # other training parameters
     scheduler=True,
-    train_test_ratio=0.8,
+    train_test_ratio=0.5,
     batch_size=-1,
     sequence_length=-1,
     bagging=True,
